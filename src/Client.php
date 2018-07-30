2f6369035c2022fcf268817cd91c1ad2f7ab4ae2
Matthew Moore <mmoore@glnndevins.com>
2018-07-29 21:43:11 -0500

    Several more Member endpoints added.

--- src/Client.php
+++ src/Client.php
@@ -22,6 +22,40 @@ class Client
      **********************************************************/
 
     /**
+     * @documentation https://projects.propublica.org/api-docs/congress-api/members/#get-members-leaving-office
+     * @endpoint https://api.propublica.org/congress/v1/{congress}/{chamber}/members/leaving.json
+     *
+     * @description Get all members of a specific congress and chamber that are leaving office.
+     * @param $congress
+     * @param $chamber - Accepted values are "senate" and "house"
+     * @return mixed
+     * @throws Exception
+     * @todo Write test
+     */
+    public function getLeavingMembers($congress, $chamber)
+    {
+        $endpoint = "{$congress}/{$chamber}/members/leaving.json";
+        return $this->makeCall($endpoint);
+    }
+
+    /**
+     * @documentation https://projects.propublica.org/api-docs/congress-api/members/#get-members-leaving-office
+     * @endpoint https://api.propublica.org/congress/v1/{congress}/{chamber}/members/leaving.json
+     *
+     * @description Get all the vote positions of a member.
+     *              Data set includes specific information about each Bill and if it passed.
+     * @param $memberId
+     * @return array
+     * @throws Exception
+     * @todo Write Test
+     */
+    public function getMemberVotePositions($memberId)
+    {
+        $endpoint ="members/{$memberId}/votes.json";
+        return $this->makeCall($endpoint);
+    }
+
+    /**
      * @documentation https://projects.propublica.org/api-docs/congress-api/members/
      * @endpoint https://api.propublica.org/congress/v1/{congress}/{chamber}/members.json
      *
@@ -30,12 +64,45 @@ class Client
      * @param $chamber - Accepted values are "senate" and "house"
      * @return mixed
      * @throws Exception
+     * @todo Write test
      */
     public function getMembers($congress, $chamber)
     {
         $endpoint = "{$congress}/{$chamber}.members.json";
         return $this->makeCall($endpoint);
     }
+
+    /**
+     * @documentation https://projects.propublica.org/api-docs/congress-api/members/#get-current-members-by-statedistrict
+     * @endpoint https://api.propublica.org/congress/v1/members/{chamber}/{state}/current.json
+     *
+     * @param $district
+     * @return mixed
+     * @throws Exception
+     * @todo Write test
+     */
+    public function getMembersByDistrict($district)
+    {
+        $endpoint = "members/house/{$district}/current.json";
+        return $this->makeCall($endpoint);
+    }
+
+
+    /**
+     * @documentation https://projects.propublica.org/api-docs/congress-api/members/#get-current-members-by-statedistrict
+     * @endpoint https://api.propublica.org/congress/v1/members/{chamber}/{state}/current.json
+     *
+     * @param $state
+     * @return mixed
+     * @throws Exception
+     * @todo Write test
+     */
+    public function getMembersByState($state)
+    {
+        $endpoint = "members/senate/{$state}/current.json";
+        return $this->makeCall($endpoint);
+    }
+
 
     /**
      * @documentation https://projects.propublica.org/api-docs/congress-api/members/#get-a-specific-member
@@ -45,10 +112,24 @@ class Client
      * @param $memberId
      * @throws Exception
      * @return mixed - Expected
+     * * @todo Write test
      */
     public function getMember($memberId)
     {
         $endpoint = "member/{$memberId}.json";
+        return $this->makeCall($endpoint);
+    }
+
+    /**
+     * @documentation https://projects.propublica.org/api-docs/congress-api/members/#get-new-members
+     * @endpoint https://api.propublica.org/congress/v1/members/new.json     *
+     * @return array
+     * @throws Exception
+     * @todo Write test
+     */
+    public function getNewMembers()
+    {
+        $endpoint = "members/new.json";
         return $this->makeCall($endpoint);
     }
 
